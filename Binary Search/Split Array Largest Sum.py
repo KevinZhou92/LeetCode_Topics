4d75abd45ac0f873fdeb39225a839219763aaddc
Pengcheng Zhou <monsterzpc@gmail.com>
2022-01-27 00:10:44 -0800

    First hard by mysefl

--- Binary Search/Split Array Largest Sum.py
+++ Binary Search/Split Array Largest Sum.py
@@ -84,3 +84,54 @@ class Solution:
         mems[(start_index, required_sum_count)] = combination_sums
 
         return combination_sums
+
+
+"""
+Solution 2:
+Binary Search + Loop(First hard question solved by myself!)
+
+Using Binary Search to find the minimum largest sum. The start would be 0 and end would be the sum of the array
+since the largest sum is the sum of the array itself.
+
+For every possible guess, we will iterate through the array and check if we can find such a split that each subarray
+of the array has a sum that is less than the guessed sum.
+
+
+Time Complexity: O(nlgW), where W is the sum of the array and n is the legth of the array
+Space complexity : O(1)
+
+"""
+
+
+class Solution:
+    def splitArray(self, nums: List[int], m: int) -> int:
+        start, end = 0, sum(nums)
+        while start + 1 < end:
+            guess = start + (end - start) // 2
+            if self.possible(guess, nums, m):
+                end = guess
+            else:
+                start = guess
+
+        if self.possible(start, nums, m):
+            return start
+
+        return end
+
+    def possible(self, guess, nums, required_count):
+        tmp_sum, index = 0, 0
+        while index < len(nums):
+            if nums[index] + tmp_sum <= guess:
+                tmp_sum += nums[index]
+            else:
+                tmp_sum = nums[index]
+                required_count -= 1
+            index += 1
+
+            if tmp_sum > guess:
+                return False
+
+            if required_count == 1:
+                return sum(nums[index - 1:]) <= guess
+
+        return True
