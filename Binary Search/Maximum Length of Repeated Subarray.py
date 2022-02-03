909686a3f7cd748e642664a5e47f2bfd42bee812
Pengcheng Zhou <monsterzpc@gmail.com>
2022-02-03 02:25:32 -0800

    DFS + Memorization

--- Binary Search/Maximum Length of Repeated Subarray.py
+++ Binary Search/Maximum Length of Repeated Subarray.py
@@ -29,3 +29,47 @@ class Solution:
             count += 1
 
         return count
+
+"""
+Solution 2:
+DFS + Memorization
+
+!!!Notice here we are looking for contiguous subarray, not for disconnect sequence.
+
+So for index1, index2, the sub problem is what is the Maximum Length of Repeated Subarray in subarray
+nums1[index1:] and nums2[index2:], if nums1[index1] == nums2[index2], then the result is 1 + output
+from sub problem nums1[index1 + 1:] and nums2[index2 + 2:], else we just return 0, which means it is not 
+possible to extend the same subarray at index1, index2, so we return to the caller stack.
+
+Time Complexity: O()
+Space complexity : O()
+"""
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        if not nums1 or not nums2:
+            return 0
+            
+        res = 0
+        mems = [[-1] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
+        for index1 in range(len(nums1)):
+            for index2 in range(len(nums2)):
+                if len(nums1) - index1 + 1 < res or len(nums2) - index2 + 1 < res:
+                    continue
+                res = max(res, self.search(nums1, nums2, index1, index2, mems))
+                
+        return res
+    
+    def search(self, nums1, nums2, index1, index2, mems):   
+        if mems[index1][index2] != -1:
+            return mems[index1][index2]
+        
+        if index1 == len(nums1) or index2 == len(nums2):
+            mems[index1][index2] = 0
+            return 0
+            
+        if nums1[index1] == nums2[index2]:
+            mems[index1][index2] = self.search(nums1, nums2, index1 + 1, index2 + 1, mems) + 1
+            return mems[index1][index2]
+        
+        mems[index1][index2] = 0
+        return 0
