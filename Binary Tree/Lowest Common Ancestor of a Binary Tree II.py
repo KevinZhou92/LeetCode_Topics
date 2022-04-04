c7acd816e316e35a7d46070c7eaee778cc0f7efb
Pengcheng Zhou <monsterzpc@gmail.com>
2022-04-04 10:48:58 -0700

    LCA ||

--- Binary Tree/Lowest Common Ancestor of a Binary Tree II.py
+++ Binary Tree/Lowest Common Ancestor of a Binary Tree II.py
@@ -48,3 +48,41 @@ class Solution:
             return root
 
         return left if left else right
+
+
+"""
+Solution 2:
+
+Compare to the first question, this question doesn't guarantee that both nodes exist in the tree.
+So we need to traverse the entire tree, which means we need to use post order traversal, only after we examine
+the result from both left tree and right tree will we return the result.
+
+Time Complexity: O(n)
+Space complexity : O(n)
+"""
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or not p or not q:
+            return False
+        self.count = 0
+        res = self.search(root, p, q)
+
+        return res if self.count == 2 else None
+
+    def search(self, root, p, q):
+        if not root:
+            return None
+
+        left = self.search(root.left, p, q)
+        right = self.search(root.right, p, q)
+
+        if left and right:
+            return root
+
+        if root == p or root == q:
+            self.count += 1
+            return root
+
+        return left if left else right
