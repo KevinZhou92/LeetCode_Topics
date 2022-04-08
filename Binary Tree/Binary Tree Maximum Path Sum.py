8b394aaad97a959197349a38ca3eb5063f912983
Pengcheng Zhou <monsterzpc@gmail.com>
2022-04-08 09:10:39 -0700

    Update

--- Binary Tree/Binary Tree Maximum Path Sum.py
+++ Binary Tree/Binary Tree Maximum Path Sum.py
@@ -16,17 +16,41 @@ class Solution:
             return 0
 
         self.globalMax = float('-inf')
-        self.search(root)
+        self.findMax(root)
 
         return self.globalMax
 
-    def search(self, root):
+    def findMax(self, root):
         if not root:
             return float('-inf')
 
-        leftMax = self.search(root.left)
-        rightMax = self.search(root.right)
+        leftMax = self.findMax(root.left)
+        rightMax = self.findMax(root.right)
         self.globalMax = max(self.globalMax, leftMax, rightMax, leftMax + root.val,
                              rightMax + root.val, leftMax + root.val + rightMax, root.val)
 
         return max(leftMax + root.val, rightMax + root.val, root.val)
+
+
+class Solution2:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        self.globalMax = float('-inf')
+        self.findMax(root)
+
+        return self.globalMax
+
+    def findMax(self, root):
+        if not root:
+            return 0
+
+        # If max left sum is 0, we will just abandon it
+        leftMax = max(self.findMax(root.left), 0)
+        rightMax = max(self.findMax(root.right), 0)
+        pathMaxSum = leftMax + root.val + rightMax
+
+        self.globalMax = max(self.globalMax, pathMaxSum)
+
+        return root.val + max(leftMax, rightMax)
