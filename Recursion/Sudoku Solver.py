6dbd87f51862c0f0ca650dd959d2086c838ea1bc
Pengcheng Zhou <monsterzpc@gmail.com>
2022-06-14 00:33:10 -0700

    Online Solution

--- Recursion/Sudoku Solver.py
+++ Recursion/Sudoku Solver.py
@@ -120,3 +120,76 @@ class Solution:
                     return False
 
         return True
+
+
+"""
+Solution 3:
+
+Recursion Simplified and Optimized for checking a grid's validity 
+Used Set to cache result so we can get O(1) time complexity while checking if a grid is valid
+for placing a number or not. This is slightly better than O(9)
+
+https://leetcode.com/problems/sudoku-solver/solution/
+
+Time Complexity: O()
+Space complexity : O()
+"""
+
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        n = len(board)
+
+        rows, cols, boxes = collections.defaultdict(
+            set), collections.defaultdict(set), collections.defaultdict(set)
+
+        for r in range(n):
+            for c in range(n):
+                if board[r][c] == '.':
+                    continue
+
+                v = int(board[r][c])
+                rows[r].add(v)
+                cols[c].add(v)
+                boxes[(r // 3) * 3 + c // 3].add(v)
+
+        def is_valid(r, c, v):
+            box_id = (r // 3) * 3 + c // 3
+            return v not in rows[r] and v not in cols[c] and v not in boxes[box_id]
+
+        def backtrack(r, c):
+            if r == n - 1 and c == n:
+                return True
+            elif c == n:
+                c = 0
+                r += 1
+
+            # current grid has been filled
+            if board[r][c] != '.':
+                return backtrack(r, c + 1)
+
+            box_id = (r // 3) * 3 + c // 3
+            for v in range(1, n + 1):
+                if not is_valid(r, c, v):
+                    continue
+
+                board[r][c] = str(v)
+                rows[r].add(v)
+                cols[c].add(v)
+                boxes[box_id].add(v)
+
+                if backtrack(r, c + 1):
+                    return True
+
+                # backtrack
+                board[r][c] = '.'
+                rows[r].remove(v)
+                cols[c].remove(v)
+                boxes[box_id].remove(v)
+
+            return False
+
+        backtrack(0, 0)
