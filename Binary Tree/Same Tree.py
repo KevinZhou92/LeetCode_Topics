ec9834be0e64ca65391ee7c2f524cdcf915cadc5
Pengcheng Zhou <monsterzpc@gmail.com>
2022-06-15 10:32:45 -0700

    Iterative Approach

--- Binary Tree/Same Tree.py
+++ Binary Tree/Same Tree.py
@@ -46,3 +46,29 @@ class Solution:
             queue.append((node1.right, node2.right))
         
         return True
+
+"""
+Solution 2-2:
+
+Iterative Approach
+Using a stack
+
+Time Complexity: O(n)
+Space complexity : O(n)
+"""
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:        
+        stack = [(p, q)]
+        
+        while stack:
+            node1, node2 = stack.pop()
+            if not node1 and not node2:
+                continue
+            if not node1 or not node2:
+                return False
+            if node1.val != node2.val:
+                return False
+            stack.append((node1.left, node2.left))
+            stack.append((node1.right, node2.right))
+        
+        return True
