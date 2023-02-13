b3606dc915ed2e6b7c318989f58a7243cc56d1f6
Pengcheng Zhou <monsterzpc@gmail.com>
2023-02-12 23:31:50 -0800

    Find all disappeared numbers

--- Arrays/Find All Numbers Disappeared in an Array.py
+++ Arrays/Find All Numbers Disappeared in an Array.py
@@ -1,6 +1,7 @@
 """
 Solution 1:
 
+Exchange Numbers
 
 Time Complexity: O(n)
 Space complexity : O(n)
@@ -26,4 +27,36 @@ class Solution:
                 res.append(index + 1)
                 
         return res
+
+
+"""
+Solution 2:
+
+Negating Numbers
+
+If a number between [1, n] show up, then we can mark index n - 1 as visited by negating the number at that index
+In the end, we can find out indicies with values greater than 0, which means the corresponding number doesn't show up
+in the array. 
+
+Time Complexity: O(n)
+Space complexity : O(n)
+"""
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+        
+        for index in range(len(nums)):
+            nextIndex = abs(nums[index]) - 1
+            
+            if nums[nextIndex] > 0:
+                nums[nextIndex] *= -1
+                    
+        res = []
+        for index, val in enumerate(nums):
+            if val > 0:
+                res.append(index + 1)
+                
+        return res
+            
             
