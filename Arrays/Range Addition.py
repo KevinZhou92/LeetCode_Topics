4068f3b6a3ce56a502f299038f6e4f89fa1e872e
Pengcheng Zhou <monsterzpc@gmail.com>
2024-06-08 23:31:12 -0700

    Update comment for diff array

--- Arrays/Range Addition.py
+++ Arrays/Range Addition.py
@@ -4,11 +4,11 @@ Solution 1:
 Build a diff array, where each value in the array represent the delta between the corresponding element and the previous element in the original array
 
 
-Time Complexity: O(n)
+Time Complexity: O(n + k) where k is the number of updates and n is the length of resulting array
 Space complexity : O(1), can be O(1) since here we are just starting with an all-zero array.
-If we have an existing array with non-zero values, then we need extra space for storing the diffs, or we could preprocess the existing array to make it 
-an diff array before starting
-
+If we have an existing array with non-zero values, then we either:
+1. Need extra space for storing the diffs
+2. Or we could preprocess the existing array to make it an diff array before starting
 """
 class Solution:
     def getModifiedArray(self, length: int, updates: List[List[int]]) -> List[int]:
